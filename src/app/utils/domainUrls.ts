bcb8e260ff9984b5c55435acb0dce4b0e974171f
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2023-07-05 03:02:09 +0300

    updated add course modal

--- src/app/utils/domainUrls.ts
+++ src/app/utils/domainUrls.ts
@@ -1,4 +1,4 @@
-const devDomain: string = "http://localhost:3000";
-const prodDomain: string = "https://quraan.onrender.com";
+const devDomain: string = 'http://localhost:3000';
+const prodDomain: string = 'https://quraan.onrender.com';
 const currentDomain: string = prodDomain;
 export default currentDomain;
